d90efb30682916d44c3656dffa83f360471fb16c
itzhetvimehta <mehta.hetvi.74@gmail.com>
2024-07-16 15:41:56 +0530

    first commit

---
+++ bubble.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+// Function to swap two elements
+void swap(int *xp, int *yp) {
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Function to implement Bubble Sort
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n-1; i++) {
+        // Last i elements are already in place
+        for (int j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+// Function to print an array
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("Sorted array: \n");
+    printArray(arr, n);
+    return 0;
+}
